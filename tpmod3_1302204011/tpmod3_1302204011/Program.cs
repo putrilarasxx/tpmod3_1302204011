42d9b49ce89aac6e670aec9fd81e5f3480897c2a
Putri Larasati <putrilarasati440@gmail.com>
2022-03-10 17:23:58 +0700

    menambahkan implementasi dengan table-driven

--- tpmod3_1302204011/tpmod3_1302204011/Program.cs
+++ tpmod3_1302204011/tpmod3_1302204011/Program.cs
@@ -10,8 +10,38 @@ namespace tpmod3_1302204011
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
+            kodePos Batununggal = new kodePos("Batunggal", 40266);
+            kodePos Kujangsari = new kodePos("Kujangsari", 40287);
+            kodePos Mengger = new kodePos("Mengger", 40267);
+            kodePos Wates = new kodePos("Wates", 40256);
+            kodePos Cijaura = new kodePos("Cijaura", 40287);
+            kodePos Jatisari = new kodePos("Kujangsari", 40286);
+            kodePos Margasari = new kodePos("Kujangsari", 40286);
+            kodePos Sekejati = new kodePos("Kujangsari", 40286);
+            kodePos Kebonwaru = new kodePos("Kujangsari", 40272);
+            kodePos Maleer = new kodePos("Kujangsari", 40274);
+            kodePos Samoja = new kodePos("Kujangsari", 40273);
+        }
+    }
+    public class kodePos
+    {
+        string kelurahan;
+        int kodepos, jumKelurahan, listKelurahan;
+
+        public kodePos(string kelurahan, int kodepos)
+        {
+            this.kelurahan = kelurahan;
+            this.kodepos = kodepos;
+        }
+        public kodePos()
+        {
+            this.kelurahan = "";
+            this.kodepos = 0;
+        }
+
+        public void getKodePos()
+        {
+            return;
         }
     }
 }
